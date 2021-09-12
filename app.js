73fdab16e865c1cd1a41b1c4524167e5879d9f30
unknown <satellite.earthresource@gmail.com>
2021-09-13 06:28:30 +0700

    second upload

--- app.js
+++ app.js
@@ -12,10 +12,152 @@ const googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z
     subdomains:['mt0','mt1','mt2','mt3']
 });
 
-// Adding Scale and Zoom Control
-const skala = L.control.scale().addTo(map);
+// Adding Scale and Zoom Control and Full Screen
+const skala = L.control.scale({
+  position: 'bottomleft',
+  metric: true,
+}).addTo(map);
+
+map.zoomControl.setPosition('topleft')
+
+// full screen mozilla---fail
+var mapId = document.getElementById('map');
+function fullScreen() {
+  mapId.full-screen-api.unprefix.enabled();
+};
+
+// Map Coordinate Mouseover
+map.on('mousemove', function (e){
+  console.log(e)
+  $('.coordinate').html(`Lat: '${e.latlng.lat} Lng: ${e.latlng.lng}`)
+});
 
 
+
+
+
+// ====================
+// Leaflet Draw Control
+// ====================
+// Default Control
+
+// var options = {
+//     position: 'topleft', // toolbar position, options are 'topleft', 'topright', 'bottomleft', 'bottomright'
+//     drawMarker: true, // adds button to draw markers
+//     drawPolyline: true, // adds button to draw a polyline
+//     drawRectangle: true, // adds button to draw a rectangle
+//     drawPolygon: true, // adds button to draw a polygon
+//     drawCircle: true, // adds button to draw a cricle
+//     cutPolygon: true, // adds button to cut a hole in a polygon
+//     editMode: true, // adds button to toggle edit mode for all layers
+//     removalMode: true, // adds a button to remove layers
+//   };
+
+//   map.pm.addControls(options);
+
+// optional options for line style during draw. These are the defaults
+var options = {
+    // snapping
+    snappable: true,
+    snapDistance: 20,
+  
+    // self intersection
+    allowSelfIntersection: true,
+  
+    // the lines between coordinates/markers
+    templineStyle: {
+      color: 'red',
+    },
+  
+    // the line from the last marker to the mouse cursor
+    hintlineStyle: {
+      color: 'red',
+      dashArray: [5, 5],
+    },
+  
+    // show a marker at the cursor
+    cursorMarker: false,
+  
+    // finish drawing on double click
+    // DEPRECATED: use finishOn: 'dblclick' instead
+    finishOnDoubleClick: false,
+  
+    // specify type of layer event to finish the drawn shape
+    // example events: 'mouseout', 'dblclick', 'contextmenu'
+    // List: http://leafletjs.com/reference-1.2.0.html#interactive-layer-click
+    finishOn: 'contextmenu',
+  
+    // custom marker style (only for Marker draw)
+    markerStyle: {
+      opacity: 0.5,
+      draggable: true,
+    },
+  };
+  
+  // enable drawing mode for shape - e.g. Poly, Line, etc
+  map.pm.enableDraw('Poly', options);
+  map.pm.enableDraw('Rectangle', options);
+  map.pm.enableDraw('Line', options);
+  map.pm.enableDraw('Marker', options);
+  map.pm.enableDraw('Circle', options);
+  
+  // get array of all available shapes
+  map.pm.Draw.getShapes();
+  
+  // listen to when drawing mode gets enabled
+  map.on('pm:drawstart', function(e) {
+    e.shape; // the name of the shape being drawn (i.e. 'Circle')
+    e.workingLayer; // the leaflet layer displayed while drawing
+  });
+  
+  // disable drawing mode
+  map.pm.disableDraw('Poly');
+  
+  // listen to when drawing mode gets disabled
+  map.on('pm:drawend', function(e) {
+    e.shape; // the name of the shape being drawn (i.e. 'Circle')
+  });
+  
+  // listen to when a new layer is created
+  map.on('pm:create', function(e) {
+    e.shape; // the name of the shape being drawn (i.e. 'Circle')
+    e.layer; // the leaflet layer created
+  });
+  
+  // listen to vertexes being added to the workingLayer (works only on polylines & polygons)
+  map.on('pm:drawstart', function(e) {
+    var layer = e.workingLayer;
+    layer.on('pm:vertexadded', function(e) {
+      // e includes the new vertex, it's marker
+      // the index in the coordinates array
+      // the working layer and shape
+    });
+  
+    // check self intersection
+    layer.pm.hasSelfIntersection();
+  });
+  
+  // listen to the center of a circle being added
+  map.on('pm:drawstart', function(e) {
+    var circle = e.workingLayer;
+  
+    // this fires only for circles
+    circle.on('pm:centerplaced', function(e) {
+      console.log(e);
+    });
+  });
+  
+  // listen to when the center of a circle is moved
+  map.on('pm:create', function(e) {
+    var circle = e.layer;
+  
+    // this fires only for circles
+    circle.on('pm:centerplaced', function(e) {
+      console.log(e);
+    });
+  });
+
+  map.pm.addControls(options);
 
 // Marker
 const firstMarker = L.marker([-7.7956,110.3695]).addTo(map)
@@ -29,9 +171,10 @@ const firstMarker = L.marker([-7.7956,110.3695]).addTo(map)
 if(!navigator.geolocation){console.log('Your browser doesnt support geolocation')
 }
 else {
-    var geoStream = setInterval(()=>{
-        navigator.geolocation.getCurrentPosition(getPosition)
-    }, 5000);
+    navigator.geolocation.getCurrentPosition(getPosition)
+    // var geoStream = setInterval(()=>{
+    //     navigator.geolocation.getCurrentPosition(getPosition)
+    // }, 5000);
 }
 // Stop streaming GeoLocation
 document.getElementById('stop-stream').onclick = function() {
@@ -90,3 +233,38 @@ if(circle){
     L.control.layers(baseMaps, overlayMaps,{
         collapsed: true
     }).addTo(map);
+
+    // Measure Control
+L.control.measure({
+   primaryLengthUnit: 'meters', 
+   secondaryLengthUnit: 'kilometers',
+   primaryAreaUnit: 'hectares',
+   secondaryAreaUnit: undefined 
+  }).addTo(map);
+
+// Geocoder
+var geocoder = L.Control.geocoder({
+  defaultMarkGeocode: false
+})
+  .on('markgeocode', function(e) {
+    var bbox = e.geocode.bbox;
+    var poly = L.polygon([
+      bbox.getSouthEast(),
+      bbox.getNorthEast(),
+      bbox.getNorthWest(),
+      bbox.getSouthWest()
+    ]).addTo(map);
+    map.fitBounds(poly.getBounds());
+  })
+
+  L.Control.geocoder().addTo(map);
+
+  // Zoom to Layer
+  $('.zoomToLyr').click(function(){
+    map.setView([-7.7956,110.3695], 10);
+  })
+
+  // Leaflet locate 
+  L.control.locate({
+    position:'topright'
+  }).addTo(map);
